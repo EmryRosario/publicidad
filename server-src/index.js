e1e74ae78131a510ad0b0ec7273eea029ab7955f
Emry Rosario <emry3008@gmail.com>
2017-09-10 19:24:49 -0400

    added react to package.json and removed engine template from server

--- server-src/index.js
+++ server-src/index.js
@@ -6,9 +6,6 @@ import React from 'react'
 const PORT = process.env.PORT || 5052
 const app = express()
 
-app.set('views', './views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('app'))
 
 app.get('/:name', (req, res) => {
